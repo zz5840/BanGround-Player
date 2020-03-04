7929db96690155ede0aa547e64ef624611876fed
青芙 <zz5840@qq.com>
2020-03-04 19:12:52 +0800

    New translations index.js (Chinese Traditional)

--- src/i18n/zh_TW/index.js
+++ src/i18n/zh_TW/index.js
@@ -26,13 +26,13 @@ export default {
             beatNote: '節奏的視覺輔助',
             mirror: '鏡像',
             laneEffect: '軌道特效',
-            resolution: '高分辨率'
+            resolution: '高解析度'
         },
         guide: {
             title: '歡迎',
             selectLanguage: '考慮到可能是第一次訪問，請選擇你喜歡的語言，稍後可以在右上角的選擇語言裡自由更改',
-            changeSettings: '第一次需要設置才能遊玩，也可在之後的設置裡隨時修改',
-            complete: '設置完畢，接下來請盡情享受吧',
+            changeSettings: '第一次需要設定才能遊玩，也可在之後的設定裡隨時修改',
+            complete: '設定完成，接下來請盡情享受吧',
             next: '下一步',
             skip: '跳過'
         }
@@ -48,7 +48,7 @@ export default {
                 type: {
                     title: '譜面類型',
                     official: '官方',
-                    community: '社區'
+                    community: '社群'
                 },
                 info: {
                     name: '歌曲名',
@@ -77,7 +77,7 @@ export default {
                 musicFile: '音樂文件'
             },
             start: '開始',
-            load: '載入信息',
+            load: '載入歌曲資訊',
             loadFailed: '載入失敗，請重試'
         },
         convert: {
@@ -92,16 +92,16 @@ export default {
             serious: '你是沙雕嗎？',
             convert: '轉換',
             error: {
-                multiTap: '在{time}处发现多押，由于目前Bestdori未支持多押，请修改后再次尝试。',
-                laneInvalid: '滑条转换出错：{time}处轨道{pos}已被占用',
-                noStart: '滑条转换出错：{time}处滑条（滑键）没有起始note'
+                multiTap: '在{time}發現多壓，由於目前Bestdori不支援多壓，請修改後再試一次',
+                laneInvalid: '滑條轉換錯誤，{time}處第{pos}道已被占用',
+                noStart: '滑條轉換錯誤，{time}處滑條（滑鍵）沒有起始拍點'
             }
         },
         space: {
             title: '計算留白',
             bpm: '第一段BPM',
             offset: '第一段Offset（秒）',
-            result: '請添加 {0} 空白到歌曲開頭或從歌曲開頭刪除 {1} 空白'
+            result: '請新增 {0} 空白到歌曲開頭或從歌曲開頭刪除 {1} 空白'
         },
         about: { title: '關於' }
     }
